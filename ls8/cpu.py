95b25c093a539c315528529898b7fe78c487df46
ChanceDurr <chancedurr@gmail.com>
2020-01-28 11:43:32 -0800

    day2 done

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,26 +7,22 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        self.ram = [0] * 8
+        self.ram = [0] * 256
         self.register = [0] * 8
         self.pc = 0
+        self.rc = 0
 
-    def load(self):
+    def load(self, program_file):
         """Load a program into memory."""
 
         address = 0
 
-        # For now, we've just hardcoded a program:
+        program = []
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        with open(str(program_file)) as f:
+            for line in f:
+                line = line.split(' ')[0].rstrip()
+                program.append(line)
 
         for instruction in program:
             self.ram[address] = instruction
@@ -69,17 +65,26 @@ class CPU:
 
         while running:
             #LDI
-            if self.ram[self.pc] == 0b10000010:
+            if self.ram[self.pc] == '10000010':
                 self.register[int(str(self.ram[self.pc + 1]), 2)] = self.ram[self.pc + 2]
                 self.pc += 3
 
             #PRN
-            elif self.ram[self.pc] == 0b01000111:
-                print(self.register[int(str(self.ram[self.pc + 1]), 2)])
+            elif self.ram[self.pc] == '01000111':
+                print(int(self.register[int(self.ram[self.pc + 1], 2)], 2))
                 self.pc += 2
 
+            #MUL
+            elif self.ram[self.pc] == '10100010':
+                a = self.register[int(str(self.ram[self.pc + 1]), 2)]
+                b = self.register[int(str(self.ram[self.pc + 2]), 2)]
+                a = int(str(a), 2)
+                b = int(str(b), 2)
+                print(a*b)
+                self.pc += 3
+
             #HLT
-            elif self.ram[self.pc] == 0b00000001:
+            elif self.ram[self.pc] == '00000001':
                 self.pc = 0
                 running = False
 
